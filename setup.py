f5c11147859134077df2e04e13c0bed7a00b1848
Aurélien Matouillot <a.matouillot@gmail.com>
2015-09-29 22:15:23 +0200

    python package

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0'
+
+setup(name='waxe.search',
+      version=version,
+      description="",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Aur\xc3\xa9lien Matouillot',
+      author_email='a.matouillot@gmail.com',
+      url='',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      test_suite='nose.collector',
+      setup_requires=['nose'],
+      tests_require=[
+        'nose',
+        'nose-cov',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
